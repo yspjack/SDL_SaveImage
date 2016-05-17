ebd0d134a92106b5cce3c0f1d233482e36f03662
yspjack <yspjack@users.noreply.github.com>
2016-05-17 00:19:49 -0500

    Add files via upload

--- SDL_SaveJPEG.cpp
+++ SDL_SaveJPEG.cpp
@@ -100,36 +100,3 @@ struct SaveJPEG
 	}
 };
 
-SaveJPEG worker;
-#define print_surface_info(surface,info) printf(#surface"->"#info"=%d\n",surface->info);
-int main()
-{
-	SDL_Init(SDL_INIT_VIDEO);
-SDL_Surface *bmp = SDL_LoadBMP("/sdcard/lena.bmp");
-	if (bmp == NULL)
-		return -1;
-	SDL_Surface *s =
-		SDL_SetVideoMode(bmp->w, bmp->h, bmp->format->BitsPerPixel, SDL_SWSURFACE | SDL_DOUBLEBUF);
-	if (s == NULL)
-		return -1;
-//	freopen("sdl.log", "w", stdout);
-//	printf("BMP BitsPerPixel=%d\n", bmp->format->BitsPerPixel);
-
-print_surface_info(bmp,w);
-print_surface_info(bmp,h);
-print_surface_info(bmp,format->BitsPerPixel);
-print_surface_info(bmp,pitch);
-
-//print_surface_info(s,w);
-//print_surface_info(s,h);
-//print_surface_info(s,format->BitsPerPixel);
-	SDL_BlitSurface(bmp, NULL, s, NULL);
-	SDL_Flip(s);
-	clock_t t=clock();
-	worker.save(s,"/sdcard/lena.jpg");
-	printf("jpeg save time: %ld\n",clock()-t);
-	fflush(stdout);
-	fclose(stdout);
-	SDL_Quit();
-	return 0;
-}
